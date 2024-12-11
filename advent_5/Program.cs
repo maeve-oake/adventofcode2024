9b25e75778d8c47bb08385ff2cc5c9b0d94b7073
hummusbird <birb@birb.cc>
2024-12-11 16:16:07 +0000

    d5p2

--- advent_5/Program.cs
+++ advent_5/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace advent_5;
+﻿using System.Transactions;
+
+namespace advent_5;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<List<int>> rules = File.ReadAllText("test.txt").Split("\n\n")[0].Split("\n").Select(x => x.Split("|").Select(int.Parse).ToList()).ToList();
-        List<List<int>> updates = File.ReadAllText("test.txt").Split("\n\n")[1].Split("\n").Select(x => x.Split(",").Select(int.Parse).ToList()).ToList();
+        List<List<int>> rules = File.ReadAllText("input.txt").Split("\n\n")[0].Split("\n").Select(x => x.Split("|").Select(int.Parse).ToList()).ToList();
+        List<List<int>> updates = File.ReadAllText("input.txt").Split("\n\n")[1].Split("\n").Select(x => x.Split(",").Select(int.Parse).ToList()).ToList();
+        List<List<int>> p2pages = [];
 
         int part1 = 0;
         int part2 = 0;
@@ -24,6 +27,31 @@ class Program
             }
 
             if (allowed) { part1 += update[update.Count / 2]; }
+            else { p2pages.Add(update); }
+        });
+
+        p2pages.ForEach(update =>
+        {
+            List<int> check = [];
+            while (!Enumerable.SequenceEqual(check, update))
+            {
+                check = update.ToList();
+                for (int page = 0; page < update.Count; page++)
+                {
+                    rules.Where(x => x[0] == update[page]).ToList().ForEach(rule =>
+                    {
+                        int found = update.FindIndex(x => x == rule[1]);
+                        if (found < page && found >= 0)
+                        {
+                            int temp = update[page];
+                            update[page] = update[found];
+                            update[found] = temp;
+                        }
+                    });
+                }
+            }
+
+            part2 += update[update.Count / 2];
         });
 
         Console.WriteLine("Part 1: " + part1);
