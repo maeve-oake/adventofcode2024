45680f67693e5c0693f1811b0da1d468187000bc
maeve <maeve@oa.ke>
2024-12-14 17:25:52 +0000

    aaaaaaaaaaaaaaaaaaaaa

--- advent_6/Program.cs
+++ advent_6/Program.cs
@@ -1,13 +1,11 @@
-﻿using System.Text;
-
-namespace advent_6;
+﻿namespace advent_6;
 
 class Program
 {
     static void Main(string[] args)
     {
         List<string> map = File.ReadAllLines("input.txt").ToList();
-        List<string> visited = route(map);
+        List<Tuple<int, int>> visited = route(map);
 
         Console.WriteLine("Part 1: " + visited.Distinct().Count());
 
@@ -15,33 +13,26 @@ class Program
         int y = map.IndexOf(map.Find(x => x.Contains('^')));
         int x = map[y].IndexOf('^');
 
-        visited.RemoveAll(j => int.Parse(j.Split(",")[0]) < y && int.Parse(j.Split(",")[1]) == x); // remove places in LOS of the guard's start
-        visited.Remove(y + "," + x);
+        visited.Remove(new Tuple<int, int>(y, x)); // remove guard's starting position
 
         int p2 = 0;
 
-        for (int i = 0; i < visited.Count(); i++)
+        for (int i = 0; i < visited.Count; i++)
         {
-            List<string> p2map = new List<string>(map);
-
-            StringBuilder temp = new StringBuilder(map[int.Parse(visited[i].Split(",")[0])]);
-            temp[int.Parse(visited[i].Split(",")[1])] = '#';
-            p2map[int.Parse(visited[i].Split(",")[0])] = temp.ToString();
-
-            Console.WriteLine(i + "/" + visited.Count());
-            if (route(p2map).Count() == 0) { p2++; };
+            if (route(map, visited[i]) is null) { p2++; };
         }
 
         Console.WriteLine("Part 2: " + p2);
     }
 
-    static List<string> route(List<string> map)
+    static List<Tuple<int, int>> route(List<string> map, Tuple<int, int>? extra = null)
     {
-        List<string> visited = [];
+        Dictionary<Tuple<int, int>, int> visited = [];
+
         int turns = 0;
         int y = map.IndexOf(map.Find(x => x.Contains('^')));
         int x = map[y].IndexOf('^');
-        visited.Add(y + "," + x);
+        visited[new Tuple<int, int>(y, x)] = 1;
 
         bool loop = true;
 
@@ -51,31 +42,38 @@ class Program
             {
                 case 1: // up
                     if (y == 0) { loop = false; break; }
-                    if (map[y - 1][x] == '#') { turns++; x++; }
+                    if (map[y - 1][x] == '#' || (extra is not null && (y - 1) == extra.Item1 && x == extra.Item2)) { turns++; x++; }
                     else { y--; }
                     break;
                 case 2: // right
-                    if (x + 1 == map[0].Count()) { loop = false; break; }
-                    if (map[y][x + 1] == '#') { turns++; y++; }
+                    if (x + 1 == map[0].Length) { loop = false; break; }
+                    if (map[y][x + 1] == '#' || (extra is not null && (y == extra.Item1) && (x + 1) == extra.Item2)) { turns++; y++; }
                     else { x++; }
                     break;
                 case 3: // down
-                    if (y + 1 == map.Count()) { loop = false; break; }
-                    if (map[y + 1][x] == '#') { turns++; x--; }
+                    if (y + 1 == map.Count) { loop = false; break; }
+                    if (map[y + 1][x] == '#' || (extra is not null && (y + 1) == extra.Item1 && x == extra.Item2)) { turns++; x--; }
                     else { y++; }
                     break;
                 case 4: // left
                     if (x == 0) { loop = false; break; }
-                    if (map[y][x - 1] == '#') { turns++; y--; }
+                    if (map[y][x - 1] == '#' || (extra is not null && (y == extra.Item1) && (x - 1) == extra.Item2)) { turns++; y--; }
                     else { x--; }
                     break;
             }
 
-            visited.Add(y + "," + x);
+            if (loop)
+            {
+                Tuple<int, int> cspace = new(y, x);
 
-            if (visited.Distinct().Count() < visited.Count() / 8) { return new List<string>(); } // just return nothing if it loops
+                int value;
+                visited.TryGetValue(cspace, out value);
+                if (value > 10) { return null; }
+
+                visited[cspace] = value + 1;
+            }
         }
 
-        return visited;
+        return visited.Keys.ToList();
     }
 }
